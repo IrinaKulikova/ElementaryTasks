9e451be5b7aaf34f90080f8c7593c49a2c8bc17d
Irina <cg.irenecooper@gmail.com>
2019-08-29 15:49:42 +0300

    commit

---
+++ Task2_Envelopes/Factories/IEnvelopeFactory.cs
@@ -0,0 +1,9 @@
+﻿using Task2_Envelopes.Models.Interfaces;
+
+namespace Task2_Envelopes.Factories
+{
+    public interface IEnvelopeFactory
+    {
+        IEnvelope Create(int heigth, int width);
+    }
+}
---
+++ Task2_Envelopes/UI/CommandManager.cs
@@ -0,0 +1,9 @@
+﻿using Task2_Envelopes.Commands;
+
+namespace Task2_Envelopes.UI
+{
+    public class CommandManager : ICommandManager
+    {
+        
+    }
+}
